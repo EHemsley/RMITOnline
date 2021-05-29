12de0d9eedf4a8f8933ad101c102d0299e3c94c0
EHemsley <82008673+EHemsley@users.noreply.github.com>
2021-05-29 20:49:20 +1000

    Update get_input_args_predict.py

--- get_input_args_predict.py
+++ get_input_args_predict.py
@@ -5,7 +5,7 @@ def get_input_args():
     parser.add_argument('--save_dir', type = str, default='checkpoint.pth', help = 'Path to checkpoint')
     parser.add_argument('--category_names',type= str, default = 'cat_to_name.json', help = 'JSON file mapping categories to real names')
     parser.add_argument('--image_path', type = str, default = 'flowers/train/32/image_05611.jpg', help='Path to image for processing')
-    parser.add_argument('--gpu', action = "store", default = 'cuda',  help = 'Chose between CPU or GPU (GPU recommended)')
+    parser.add_argument('--gpu', action = "store", default = 'gpu',  help = 'Chose between CPU or GPU (GPU recommended)')
     parser.add_argument('--topk', type = int, default = 5, help = 'Top K classes')
 
     return parser.parse_args()
